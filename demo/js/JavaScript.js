d1f10d1269773fa0ff39e8ca9d88c623fcb12b85
s96116157 <s96116157@gmail.com>
2018-01-03 16:32:26 +0800

    Fix

--- demo/js/JavaScript.js
+++ demo/js/JavaScript.js
@@ -19,14 +19,16 @@ function getLocation() {
 function showPosition(position) {
     var x = document.getElementById("map");
     var y = document.getElementById("location");
-    var latlon = position.coords.latitude.toFixed(7) + "," + position.coords.longitude.toFixed(7);
+    var n = position.coords.latitude.toFixed(7);
+    var e = position.coords.longitude.toFixed(7);
+    var latlon = n + "," + e;
     var img_url = "https://maps.googleapis.com/maps/api/staticmap?center="
     + latlon + "&zoom=18&size=600x450&markers=size:mid%7color:red%7Clabel:S%7C" + latlon;
     x.innerHTML = "<img src='" + img_url + "' style='display:block; margin: auto;'>";
     y.innerHTML = "" +
         "<ul style='list-style-type: none;'>" +
         "<li>座標位置</li>" +
-        "<li>緯度：" + position.coords.latitude.toFixed(7) + "</li>" +
-        "<li>經度：" + position.coords.longitude.toFixed(7) + "</li>" +
+        "<li>緯度：" + n + "</li>" +
+        "<li>經度：" + e + "</li>" +
         "</ul>";
 }
